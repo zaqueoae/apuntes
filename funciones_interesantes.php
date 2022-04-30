3651262399f465081fa31a52a60e9cc5973d5ecb
Zaqueo Echeverria <zaqueoae@gmail.com>
2022-04-30 11:21:07 +0200

    Update funciones_interesantes.php

--- funciones_interesantes.php
+++ funciones_interesantes.php
@@ -3,4 +3,12 @@
 //$db es es $db = mysqli_connect($server, $username, $password, $database);
 mysqli_real_escape_string($db, $_POST ['nombre']);
 
+//Cifrar la contraseña antes de guardarla
+$paswword = "micontraseña";
+$password_segura = password_hash($password), PASSWORD_BYCRYPT, ['cost'=>4]);
+
+//Comparar la contraseña que introduce con la cifrada en la base de datos
+//Si es la misma nos devuelve un true y si no coincide devuelve un false.
+password_verify($paswword, $password_segura);
+
 ?>
