8115a4965a50e53ff1f6ffd88af4ec5733d219f2
Zaqueo Echeverria <zaqueoae@gmail.com>
2022-04-30 14:55:01 +0200

    Update funciones_y _scripts_interesantes.php

--- funciones_y _scripts_interesantes.php
+++ funciones_y _scripts_interesantes.php
@@ -5,7 +5,7 @@ mysqli_real_escape_string($db, $_POST ['nombre']);
 
 //Cifrar la contraseña antes de guardarla
 $pasword = "micontraseña";
-$password_segura = password_hash($password), PASSWORD_BYCRYPT, ['cost'=>4]);
+$password_segura = password_hash($password, PASSWORD_BCRYPT, ['cost'=>4]);
 
 //Comparar la contraseña que introduce con la cifrada en la base de datos
 //Si es la misma nos devuelve un true y si no coincide devuelve un false.
